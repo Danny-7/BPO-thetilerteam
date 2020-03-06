cee05083672d9de6a6ea01a4e9624aa3f62a57b5
Danny-7 <danielaguiar@outlook.fr>
2020-03-07 00:35:05 +0100

    Nothing

--- src/fr/thetilerteam/Appli.java
+++ src/fr/thetilerteam/Appli.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Appli {
+	private static final String SCORE = "Voici votre score : ";
 
 	public static void gameMessage() {
 		System.out.println(" Pour arrêter de jouer --> exit");
@@ -23,78 +24,78 @@ public class Appli {
 			play.addRandomTile();
 		} catch (IOException e) {
 			System.out.println("Impossible !!");
-			
+
 		}
 		System.out.println("Début de partie !! Bonne chance \n");
 		System.out.println(play.toString());
 		Scanner sc = new Scanner(System.in);
 		gameMessage();
-		String choix = sc.next().trim().toLowerCase();
-		if(choix.equals("jouer")) {
+		String choix = sc.next();
+		if(choix.equalsIgnoreCase("jouer")) {
 			// Tant que le paquet de carte n'est pas vide et la liste des carreaux n'est pas vide
 			while(!play.isFinish()){
 				String chain = "";
 				// On joue tant que la chaine de caractère n'est pas "exit"
-				if (!chain.trim().toLowerCase().equals("stop")) {
+				if (!chain.equalsIgnoreCase("stop")) {
 					// On pioche une carte
 					Card c = play.pickCard();
 					System.out.println("Voici la carte piochée !" + "\t" + p.toString(c) + "\n");
 					System.out.println("Voici les carreaux disponible(s) pour la carte piochée:" + "\n" + game.toStringtab(c));
 					System.out.println("Voulez vous garder la carte ? OUI/NON");
 					chain = sc.next().trim().toUpperCase();
-					if(chain.equals("NON")) {
+					while(chain.equalsIgnoreCase("NON")) {
 						s.addCard(c);
 						c = play.pickCard();
 						System.out.println("Voici la nouvelle carte piochée !" + "\t" + p.toString(c));
 						System.out.println("Voici les carreaux dispo pour la nouvelle carte piochée:" + "\n" + game.toStringtab(c));
 						System.out.println("Voulez vous garder la carte ? OUI/NON");
-						chain = sc.next().trim().toUpperCase();
+						chain = sc.next();
 					}
-					else if(chain.equals("OUI")) {
-						System.out.println("Selectionner un carreau pour la consigne donnée [a-i][A-I] et la position (y,x)");
-						chain = sc.next().trim();
-						int y = sc.nextInt() - 1;
-						int x = sc.nextInt() - 1;
 
-						try {
-							// On essaye de poser le carreau sinon on renvoi ce message d'erreur 
-							play.addTile(y, x, game.choice(chain));
-						} catch (IllegalArgumentException | IOException e) {
-							System.out.println("Entrez des coordonnées correct où verifiez les conditions de pose !!");
-						}
-						// Affichage du mur
-						System.out.println(play.toString() + "\n");
-						System.out.println("Voulez vous continuer ou arrêter de jouer ? (stop ou jouer)");
-						chain = sc.next().trim().toLowerCase();
+					System.out.println("Selectionner un carreau pour la consigne donnée [a-i][A-I] et la position (y,x)");
+					chain = sc.next().trim();
+					int y = sc.nextInt() - 1;
+					int x = sc.nextInt() - 1;
+
+					try {
+						// On essaye de poser le carreau sinon on renvoi ce message d'erreur 
+						play.addTile(y, x, game.choice(chain));
+					} catch (IllegalArgumentException | IOException e) {
+						System.out.println("Entrez des coordonnées correct où verifiez les conditions de pose !!");
 					}
-					else
-						System.out.println("Erreur de saisie !!!!");
-					}
+					// Affichage du mur
+					System.out.println(play.toString() + "\n");
+					System.out.println("Voulez vous continuer ou arrêter de jouer ? (stop ou jouer)");
+					chain = sc.next();
+
+				}
 				else {
-					System.out.println("Voici votre score : " + s.displayScore());
+					System.out.println(SCORE + s.displayScore());
 					System.out.println("\t");
 					System.out.println(play.toString());
+					System.out.println("\n" +"La partie est terminée merci d'avoir joué");
 					// Arrêt du jeu 
 					play.exit();
 				}
-				}
 			}
-		
+		}
 		else if(choix.equals("stop")) {
-			System.out.println("Voici votre score : " + s.displayScore());
+			System.out.println(SCORE + s.displayScore());
 			System.out.println("\t");
 			System.out.println(play.toString());
-			System.out.println("\n" +"La partie est terminée merci d'avoir joué");
+			System.out.println("\n" +"La partie est terminée merci d'avoir joué !");
 			play.exit();
 
 		}
 		else {
-			
-			System.out.println("Voici votre score : " + s.displayScore());
+
+			System.out.println(SCORE  + s.displayScore());
 			System.out.println("\t");
 		}
-		
+
 		sc.close();
 	}
+
 	
 }
+	
